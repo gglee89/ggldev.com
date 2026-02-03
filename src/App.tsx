5724440be543e484c83bac62deab0e36cde33fa8
Giwoo Lee <gglee89@gmail.com>
2026-02-03 18:18:02 +0000

    feat: upgrade bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+// Router
+import { Route, Routes } from 'react-router-dom'
+
+import Home from 'pages/Home'
+
 import './App.css'
 import './Theme.css'
-
 import NotFound from './pages/NotFound'
-
-// Router
-import { Routes, Route } from 'react-router-dom'
-import Home from 'pages/Home'
 
 const App = () => {
     return (
